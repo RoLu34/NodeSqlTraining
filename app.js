a2eee34e2cd334a9ef7ca13a8fa90763cd408630
RoLu34 <rodriguezl34300@gmail.com>
2025-10-23 16:53:02 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World !');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(`Data received: ${JSON.stringify(req.body)}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
