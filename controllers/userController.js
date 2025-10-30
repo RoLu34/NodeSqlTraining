7d0b98cb4c827f418068e09adc63b2989a7ce13c
RoLu34 <rodriguezl34300@gmail.com>
2025-10-30 19:23:22 +0100

    ADD errorMessage file AND FIX response message

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
-import { isUsernameValid, isEmailValid, isPasswordValid } from "../middleware/validations/userValidation.js";
 import User from "../models/userModel.js";
+import { isUsernameValid, isEmailValid, isPasswordValid } from "../middleware/validations/userValidation.js";
+import { userError, commonErrors } from "../middleware/errorMessage.js";
 
 export const getAllUsers = async (req, res) => {
     try {
         const users = await User.findAll();
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({ error: "Failed to retrieve users." });
+        res.status(500).json({ message: commonErrors.DATABASE_UNAVAILABLE });
     }
 }
 
@@ -16,59 +17,59 @@ export const getOneUser = async (req, res) => {
         if(user){
             res.status(200).json(user);
         } else {
-            res.status(404).json({ error: "User not found." });
+            res.status(404).json({ message: userError.USER_NOT_FOUND });
         }   
     } catch (error) {
-        res.status(500).json({ error: "Failed to retrieve user." });
+        res.status(500).json({ message: commonErrors.DATABASE_UNAVAILABLE });
     }
 }
 
 export const createUser = async (req, res) => {
 
     if(!isUsernameValid(req.body.username)){
-        return res.status(400).json({ error: "Invalid username format." });
+        return res.status(400).json({ message: userError.INVALID_USERNAME_FORMAT });
     }
 
     if(!isEmailValid(req.body.email)){
-        return res.status(400).json({ error: "Invalid email format." });
+        return res.status(400).json({ message: userError.INVALID_EMAIL_FORMAT });
     }
 
     if(!isPasswordValid(req.body.password)){
-        return res.status(400).json({ error: "Invalid password format" });
+        return res.status(400).json({ message: userError.INVALID_PASSWORD_FORMAT });
     }
 
     try{
         await User.create(req.body);
-        res.status(201).json({ message: "User created successfully." });
+        res.status(201);
     } catch (error) {
-        res.status(500).json({ error: "Failed to create user." });
+        res.status(500).json({ message: commonErrors.DATABASE_UNAVAILABLE });
     }
 }
 
 export const updateUser = async (req, res) => {
 
     if(!isUsernameValid(req.body.username)){
-        return res.status(400).json({ error: "Invalid username format." });
+        return res.status(400).json({ message: userError.INVALID_USERNAME_FORMAT });
     }
 
     if(!isEmailValid(req.body.email)){
-        return res.status(400).json({ error: "Invalid email format." });
+        return res.status(400).json({ message: userError.INVALID_EMAIL_FORMAT });
     }
 
     if(!isPasswordValid(req.body.password)){
-        return res.status(400).json({ error: "Invalid password format" });
+        return res.status(400).json({ message: userError.INVALID_PASSWORD_FORMAT });
     }
 
     try{
         const user = await User.findByPk(req.params.id);
         if(user){
             await user.update(req.body);
-            res.status(200).json({ message: "User updated"});
+            res.status(200);
         } else{
-            res.status(404).json({ error: "User not found." });
+            res.status(404).json({ message: userError.USER_NOT_FOUND });
         }
     } catch (error) {
-        res.status(500).json({ error: "Failed to update user." });
+        res.status(500).json({ message: commonErrors.DATABASE_UNAVAILABLE });
     }
 }
 
@@ -77,11 +78,11 @@ export const deleteUser = async (req, res) => {
         const user = await User.findByPk(req.params.id);
         if(user){
             await user.destroy();
-            res.status(200).json({ message: "User deleted"});
+            res.status(200);
         } else{
-            res.status(404).json({ error: "User not found." });
+            res.status(404).json({ message: userError.USER_NOT_FOUND });
         }
     } catch (error) {
-        res.status(500).json({ error: "Failed to delete user." });
+        res.status(500).json({ message: commonErrors.DATABASE_UNAVAILABLE });
     }
 }
